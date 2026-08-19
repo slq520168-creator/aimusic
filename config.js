204007b97fc736ce4f2a6f040387ae89da112ad9
slq520168-creator <slq520168@gmail.com>
2026-08-20 02:01:38 +0800

    Centralize AI Music runtime configuration

---
+++ config.js
@@ -0,0 +1,15 @@
+window.AIMUSIC_CONFIG=Object.freeze({
+  SUPABASE_URL:'https://afzcohtnljnmucrkgcaz.supabase.co',
+  SUPABASE_KEY:'sb_publishable_EqF-kTNRsSZWhUE8LWB8DQ_UNkTjImv',
+  STORAGE_BUCKET:'aimusic-audio',
+  MAIN_SITE:'https://globalyouxuan-order.pages.dev',
+  FUNCTIONS:Object.freeze({
+    GUEST_POINTS:'aimusic-guest-points',
+    SUBMIT_TRACK:'aimusic-submit-track',
+    LYRICS:'aimusic-lyrics',
+    VOCAL_GENERATE:'aimusic-vocal-generate',
+    GENERATE_AUDIO:'aimusic-generate-audio',
+    ADMIN:'aimusic-admin',
+    ADMIN_UPLOAD:'aimusic-admin-upload'
+  })
+});
